6f45e75dd8e6936d19b48a5f402e120e4eab3c45
Ionuț-Claudiu (John) Panțu-Irimia <John@in-fact.com>
2024-01-15 00:32:32 +0000

    feat: initial commit; added api stub

---
+++ CompanyClaims.Api/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CompanyClaims API", Description = "Interact with test insurance data", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyClaims API v1");
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
